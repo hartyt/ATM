cfb9e2679d16b9006571852d2532adcf7bf5ed7f
Thomas <Thomas@IBM979-R9YG70F>
2015-01-17 18:30:14 +0000

    first checkin

---
+++ atm/src/atm/Test.java
@@ -0,0 +1,23 @@
+/*
+ * Test.java
+ * Created on 17 Jan 2015 at 17:36:44
+ * 
+ * ====================================================================
+ * Licensed Materials - Property of IBM                            
+ * 5724-B88 
+ * © Copyright IBM Corp.  2007, 2015
+ * All Rights Reserved. 
+ *                                                                 
+ * US Government Users Restricted Rights - Use, duplication or
+ * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ * ====================================================================
+ * 
+ */
+package atm;
+
+/**
+ * Test
+ */
+public class Test {
+
+}
